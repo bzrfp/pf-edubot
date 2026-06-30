6ff840458675c339704cba86348877ddbe299f35
bzrfp <btrz.rfp@gmail.com>
2026-06-30 16:59:01 -0300

    Create teste01_segue_linha.ino

---
+++ teste01.ino
@@ -0,0 +1,36 @@
+#include <Sparki.h> 
+
+void setup() 
+{
+}
+
+void loop() {
+  int lim = 500;
+
+  int lineLeft   = sparki.lineLeft();   
+  int lineCenter = sparki.lineCenter(); 
+  int lineRight  = sparki.lineRight();  
+
+  if ( lineLeft < lim ) 
+  {  
+    sparki.moveLeft(); 
+  }
+
+
+  else if (lineCenter < lim) 
+  {  
+    sparki.moveForward(); 
+  }
+
+  else if ( lineRight < lim )
+  {
+    sparki.moveRight(); 
+  }  
+  
+  else
+  {
+    sparki.moveRight(180);
+  }
+
+  delay(100); 
+}
