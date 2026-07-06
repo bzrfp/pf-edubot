4cfbc7915a9f17e458002b64f37e347b0e453869
bzrfp <btrz.rfp@gmail.com>
2026-07-06 12:36:03 -0300

    Create 02-contador.ino

---
+++ 02-contador.ino
@@ -0,0 +1,44 @@
+//é bom printar essa variavel no LCD pra ver se a contagem tá certa
+
+#include <Sparki.h>
+
+int  pontoSemSaida = 0;  
+
+void setup() {
+}
+
+void loop() {
+  int lim = 500;
+  int lineLeft   = sparki.lineLeft();
+  int lineCenter = sparki.lineCenter();
+  int lineRight  = sparki.lineRight();
+
+  bool achouLinha = true; 
+
+  if ( lineLeft < lim )
+  {
+    sparki.moveLeft();
+  }
+  else if ( lineCenter < lim )
+  {
+    sparki.moveForward();
+  }
+  else if ( lineRight < lim )
+  {
+    sparki.moveRight();
+  }
+  else
+  {
+    // ponto sem saída
+    achouLinha = false;
+    sparki.moveRight(180);
+  }
+
+  //contador
+  if ( !achouLinha )
+  {
+    pontoSemSaida++;
+  }
+  
+  delay(100);
+}
